7abe416f2e905b9ae9384b3320eeb596e184cd98
Zenaida <zenaidamariscal1@gmail.com>
2024-08-07 23:16:05 -0500

    xml file changes

--- MortgageCalculator/app/src/main/java/com/example/mortgagecalculator/MainActivity.kt
+++ MortgageCalculator/app/src/main/java/com/example/mortgagecalculator/MainActivity.kt
@@ -1,10 +1,22 @@
 package com.example.mortgagecalculator
 
-import ...
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.testapp.ui.theme.TestAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main
     }
 }
+
