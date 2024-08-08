bd9b686743babe59553b83a7fdb8c085e5576a63
Zenaida <zenaidamariscal1@gmail.com>
2024-08-07 23:22:22 -0500

    xml file changes

--- MortgageCalculator/app/src/main/java/com/example/mortgagecalculator/MainActivity.kt
+++ MortgageCalculator/app/src/main/java/com/example/mortgagecalculator/MainActivity.kt
@@ -11,12 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.testapp.ui.theme.TestAppTheme
+import com.example.mortgagecalculator.ui.theme.MortgageCalculatorTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main
+        setContentView(R.layout.activity_main)
     }
 }
 
