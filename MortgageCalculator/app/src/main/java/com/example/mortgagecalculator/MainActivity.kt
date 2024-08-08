9931413c7d81d671aed537ebd6631f3e7db9f219
wond2k4 <wonderakp04@gmail.com>
2024-08-08 17:10:53 -0400

    ]

--- MortgageCalculator/app/src/main/java/com/example/mortgagecalculator/MainActivity.kt
+++ MortgageCalculator/app/src/main/java/com/example/mortgagecalculator/MainActivity.kt
@@ -12,12 +12,26 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mortgagecalculator.ui.theme.MortgageCalculatorTheme
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), View.OnClickListener {
+    lateinit var btnSubmit : Button
+    lateinit var btnCreateAccount:  Button
+    lateinit var btnResetPassword: Button
+    lateinit var etPass: EditText
+    lateinit var etUser: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
     }
 }
 
